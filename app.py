ca45a98b718370bd4859142cbb774eb24a66940d
shiroelabo <shiroelabo@gmail.com>
2025-05-20 07:57:54 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -99,7 +99,6 @@ with st.form(key="counseling_form"):
     selected_style = [opt for opt in style_options if st.checkbox(opt, key=opt+"style")]
 
     concerns = st.markdown("**その他、気になること・不安など**")
-    st.markdown("**その他、気になること・不安など**")
     concerns = st.text_area("")
 
     st.subheader("■ 同意事項")
